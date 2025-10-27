e2a9a4373120e12df6a9fc2546d5d6d4377c1ec6
hamed <abdulrahman.hamed914@gmail.com>
2025-10-28 00:12:52 +0300

    adjust property icons alignment and size

--- src/app/[locale]/PropertiesList/components/PropertyAvailabilityTypes.tsx
+++ src/app/[locale]/PropertiesList/components/PropertyAvailabilityTypes.tsx
@@ -15,43 +15,49 @@ const PropertyAvailabilityTypes = ({ item }: Props) => {
     <>
       {isMobile ? (
         <div
-          className={`flex flex-row px-[10%] ${
+          className={`flex flex-row px-[5%] ${
             item.type != "SEPARATED" ? "justify-between" : "justify-center"
           } border-t-1 border-t-gray-300 py-[2vmin]`}
         >
-          <div>
+          <div className="items-center flex flex-col">
             <AvailabilityPopoverButton
               type="Apartment"
               item={item.project.apartments}
-              className="w-[8vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
+              className="w-[12vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
             >
-              <i className="fa-solid fa-city text-primary text-4xl"></i>
+              <i className="fa-solid fa-city text-primary text-1xl"></i>
             </AvailabilityPopoverButton>
-            <p className="text-center font-bold mt-2">{t("Apartment")}</p>
+            <p className="text-center text-sm font-bold mt-2">
+              {t("Apartment")}
+            </p>
           </div>
 
           {item.type != "SEPARATED" && (
             <>
-              <div>
+              <div className="items-center flex flex-col">
                 <AvailabilityPopoverButton
                   type="Villa"
                   item={item.project.villas}
-                  className="w-[8vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
+                  className="w-[12vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
                 >
-                  <i className="fa-solid fa-house-chimney text-primary text-4xl"></i>
+                  <i className="fa-solid fa-house-chimney text-primary text-1xl"></i>
                 </AvailabilityPopoverButton>
-                <p className="text-center font-bold mt-2">{t("Villa")}</p>
+                <p className="text-center text-sm font-bold mt-2">
+                  {t("Villa")}
+                </p>
               </div>
 
-              <div>
+              <div className="items-center flex flex-col">
                 <AvailabilityPopoverButton
                   type="Commercial"
                   item={item.project.mall}
-                  className="w-[8vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
+                  className="w-[12vmin] h-[12vmin] rounded-full hover:bg-primary/5 cursor-pointer shadow-md"
                 >
-                  <i className="fa-solid fa-hotel text-primary text-4xl"></i>
+                  <i className="fa-solid fa-hotel text-primary text-1xl"></i>
                 </AvailabilityPopoverButton>
-                <p className="text-center font-bold mt-2">{t("Commercial")}</p>
+                <p className="text-center text-sm font-bold mt-2">
+                  {t("Commercial")}
+                </p>
               </div>
             </>
           )}
@@ -60,41 +66,41 @@ const PropertyAvailabilityTypes = ({ item }: Props) => {
         <div
           className={`flex flex-row px-[10%] ${
             item.type != "SEPARATED" ? "justify-between" : "justify-center"
-          } border-t-1 border-t-gray-300 py-[2vmin]`}
+          } border-t-1 border-t-gray-300 py-[1vmin]`}
         >
-          <div>
+          <div className="items-center flex flex-col">
             <AvailabilityPopoverButton
               type="Apartment"
               item={item.project.apartments}
-              className="w-[10vmin] h-[10vmin] rounded-[5vmin] hover:bg-primary/5 cursor-pointer shadow-md"
+              className="w-[6vmin] h-[6vmin] rounded-[7vmin] hover:bg-primary/5 cursor-pointer shadow-md"
             >
-              <i className="fa-solid fa-city text-primary text-5xl"></i>
+              <i className="fa-solid fa-city text-primary text-2xl"></i>
             </AvailabilityPopoverButton>
             <p className="text-center font-bold mt-2">{t("Apartment")}</p>
           </div>
 
           {item.type != "SEPARATED" && (
             <>
-              <div>
+              <div className="items-center flex flex-col">
                 <AvailabilityPopoverButton
                   type="Villa"
                   item={item.project.villas}
-                  className="w-[10vmin] h-[10vmin] rounded-[5vmin] hover:bg-primary/5 cursor-pointer shadow-md"
+                  className="w-[6vmin] h-[6vmin] rounded-[7vmin] hover:bg-primary/5 cursor-pointer shadow-md"
                 >
-                  <i className="fa-solid fa-house-chimney text-primary text-5xl"></i>
+                  <i className="fa-solid fa-house-chimney text-primary text-2xl"></i>
                 </AvailabilityPopoverButton>
                 <p className="text-center font-bold mt-2">{t("Villa")}</p>
               </div>
 
-              <div>
+              <div className="items-center flex flex-col">
                 <AvailabilityPopoverButton
                   type="Commercial"
                   item={item.project.mall}
-                  className="w-[10vmin] h-[10vmin] rounded-[5vmin] hover:bg-primary/5 cursor-pointer shadow-md"
+                  className="w-[6vmin] h-[6vmin] rounded-[7vmin] hover:bg-primary/5 bg-background cursor-pointer shadow-md"
                 >
-                  <i className="fa-solid fa-hotel text-primary text-5xl"></i>
+                  <i className="fa-solid fa-hotel text-primary text-2xl"></i>
                 </AvailabilityPopoverButton>
-                <p className="text-center font-bold mt-2">{t("Commercial")}</p>
+                <p className="text-center  font-bold mt-2">{t("Commercial")}</p>
               </div>
             </>
           )}
